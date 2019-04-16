62723be480c3e04c3348d6c31c50e9633701378b
Sungho,Yun <itlockit@gmail.com>
2019-04-16 14:04:34 +0900

    finish study

---
+++ recommended song/main.py
@@ -0,0 +1,14 @@
+import pandas
+from sklearn.model_selection  import train_test_split
+import numpy as np
+import time
+from sklearn.externals import joblib
+
+# load song data
+## load user_id-song, id-listen_count
+triplets_file = 'https://static.turi.com/datasets/millionsong/10000.txt'
+songs_metadata_file = 'https://static.turi.com/datasets/millionsong/song_data.csv'
+
+song_df_1 = pandas
+
+
